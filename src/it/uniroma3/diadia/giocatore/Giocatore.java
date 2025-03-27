fd0a7e9e9cc3ed05f5e85c289eb2fe3311b4a020
brunett0 <mat.pardo@stud.uniroma3.it>
2025-03-27 17:46:58 +0100

    Add files via upload

---
+++ src/it/uniroma3/diadia/giocatore/Giocatore.java
@@ -0,0 +1,30 @@
+package it.uniroma3.diadia.giocatore;
+
+public class Giocatore {
+	static final private int CFU_INIZIALI = 20;
+	private int cfu;
+	private Borsa borsa=new Borsa();
+	
+	public Giocatore() {
+		cfu=CFU_INIZIALI;
+		
+	}
+
+	public int getCfu() {
+		return cfu;
+	}
+
+	public void setCfu(int cfu) {
+		this.cfu = cfu;
+	}
+
+	public Borsa getBorsa() {
+		return borsa;
+	}
+
+	public void setBorsa(Borsa borsa) {
+		this.borsa = borsa;
+	}
+	
+	
+}
